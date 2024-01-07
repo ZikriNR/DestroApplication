33b980f610de72c76b6629e3e29c59188936bca8
Zikri Nur Rahman <zikrinr16@gmail.com>
2024-01-07 11:31:20 +0700

    upload semua file

---
+++ app/src/main/java/com/destro/ui/riwayat/RiwayatViewModel.kt
@@ -0,0 +1,13 @@
+package com.destro.ui.riwayat
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RiwayatViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is riwayat Fragment"
+    }
+    val text: LiveData<String> = _text
+}
